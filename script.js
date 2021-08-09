31f2f81f1a65366f4324cb2e03fbe6fed95c39e7
Jessie Bakashika <jessie.bakashika@gmail.com>
2021-08-09 17:23:54 +0200

    creation du site web

---
+++ script.js
@@ -0,0 +1,21 @@
+class utilisateur (username, password)
+{
+	constructor(username, password)
+{
+	this.username =""
+	this.password = ""
+};
+
+
+}; 
+
+function Connect()
+{
+current_user = utilisateur()
+current_user.username = document.getElementsByName("username"))
+current_user.password = document.getElementsByName("password")
+
+
+};
+
+Connect()
